b5888f7419a00cc07daadfd31559c3a8eaeed4f6
joerneu <31096117+joerneu@users.noreply.github.com>
2019-10-31 16:46:09 +0100

    Set global CSS

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -11,7 +11,50 @@ const SiteTheme = Theme.SiteTheme;
 exports.wrapRootElement = ({ element }) => {
     return (
         <ThemeProvider theme={SiteTheme}>
-            <Global styles={{ body: { backgroundColor: SiteTheme.colors.background, color: SiteTheme.colors.text } }} />
+            <Global
+                styles={{
+                    html: { fontSize: Theme.BaseRootFontSize },
+                    // Fix for elements inside headings not displaying properly
+                    "a, b, del, em, i, ins, q, span, strong, u": {
+                        fontSize: "1em"
+                    },
+                    "html, *": {
+                        fontFamily: Theme.BaseFontFamily,
+                        lineHeight: Theme.BaseLineHeight,
+                        // Prevent adjustments of font size after orientation changes in mobile.
+                        "-webkit-text-size-adjust": "100%"
+                    },
+                    // "*": {
+                    //     fontSize: Theme.BaseFontSize
+                    // },
+                    body: { margin: 0, color: SiteTheme.colors.text, backgroundColor: SiteTheme.colors.background },
+                    // Correct display for Edge & Firefox.
+                    details: {
+                        display: "block"
+                    },
+                    // Correct display in all browsers
+                    summary: {
+                        display: "list-item"
+                    },
+                    // Abbreviations
+                    "abbr[title]": {
+                        // Remove bottom border in Firefox 39-
+                        borderBottom: "none",
+                        // Opinionated style-fix for all browsers
+                        textDecoration: "underline dotted"
+                    },
+                    // Show overflow in Edge.
+                    input: {
+                        overflow: "visible"
+                    },
+                    // Prevent elements from affecting the line height in all browsers
+                    "sup, sub, code, kbd": {
+                        lineHeight: 0,
+                        position: "relative",
+                        verticalAlign: "baseline"
+                    }
+                }}
+            />
             {element}
         </ThemeProvider>
     );
